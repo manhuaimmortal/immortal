a26e84d40db173a2f62b2287f687fa14617651ca
nahuel <mvkjdolgjsa11@gmail.com>
2021-07-12 13:47:34 -0300

    proyecto completo

---
+++ cargaImg.js
@@ -0,0 +1,82 @@
+let ultimo=334;
+
+
+
+
+var titulo= document.title;
+    console.log(titulo);
+    var numTitulo=titulo.split("-");
+
+    numero= parseInt(numTitulo[1],10);
+
+    var mas=numero+1;
+    var menos=numero-1;
+
+    
+
+
+const contenedor=document.getElementById('contenedor');
+
+
+ let pag=window.paginas;
+var contador=0;
+
+
+
+agregaBotones()
+while(contador<=pag){
+    const imagen=document.createElement('img');
+    imagen.src='../img/'+numero+'/'+contador+'.jpg';
+    imagen.classList.add('paginas');
+    contenedor.appendChild(imagen);
+    contador++;
+}
+
+function agregaBotones(){
+    const contenedorBotones=document.querySelector('.botones');
+    if(numero==316){
+        var ir='../index.html'
+        var textobtn='caps'
+    }else{
+        var ir='cap-'+menos+'.html'
+        var textobtn='<'+menos
+        const atras= document.createElement('a');
+    atras.href=ir;
+    atras.textContent=textobtn;
+    atras.classList.add('btn-link');
+    contenedorBotones.appendChild(atras);
+    contenedor.appendChild(contenedorBotones);
+    }
+    
+    
+
+
+
+
+    const menu= document.createElement('a');
+    menu.href='../index.html';
+    menu.textContent='caps';
+    menu.classList.add('btn-link');
+    contenedorBotones.appendChild(menu);
+    contenedor.appendChild(contenedorBotones);
+
+
+
+    if(numero==ultimo){
+        
+    }else{
+        var ir='cap-'+mas+'.html'
+        var textobtn=mas+'>'
+        const adelante= document.createElement('a');
+        adelante.href=ir;
+        adelante.textContent=textobtn;
+        adelante.classList.add('btn-link');
+        contenedorBotones.appendChild(adelante);
+        contenedor.appendChild(contenedorBotones);
+    }
+
+   
+}
+
+
+agregaBotones();
---
+++ lista.js
@@ -0,0 +1,48 @@
+
+const lista = document.getElementById('lista');
+
+
+var arr = [
+['GreatImmortal/cap-316.html', 'capitulo 316'],
+['GreatImmortal/cap-317.html', 'capitulo 317'],
+['GreatImmortal/cap-318.html', 'capitulo 318'],
+['GreatImmortal/cap-319.html', 'capitulo 319'],
+['GreatImmortal/cap-320.html', 'capitulo 320'],
+['GreatImmortal/cap-321.html', 'capitulo 321'],
+['GreatImmortal/cap-322.html', 'capitulo 322'],
+['GreatImmortal/cap-323.html', 'capitulo 323'],
+['GreatImmortal/cap-324.html', 'capitulo 324'],
+['GreatImmortal/cap-325.html', 'capitulo 325'],
+['GreatImmortal/cap-326.html', 'capitulo 326'],
+['GreatImmortal/cap-327.html', 'capitulo 327'],
+['GreatImmortal/cap-328.html', 'capitulo 328'],
+['GreatImmortal/cap-329.html', 'capitulo 329'],
+['GreatImmortal/cap-330.html', 'capitulo 330'],
+['GreatImmortal/cap-331.html', 'capitulo 331'],
+['GreatImmortal/cap-332.html', 'capitulo 332'],
+['GreatImmortal/cap-333.html', 'capitulo 333'],
+['GreatImmortal/cap-334.html', 'capitulo 334']];
+
+
+
+
+
+arr.forEach(element => {
+    creaLink(element[0], element[1]);
+})
+
+function creaLink(href, texto) {
+    const lin = document.createElement('a');
+    lin.classList.add('cap-link');
+    lin.href = href;
+    lin.textContent = texto;
+
+    creaElementos(lin);
+}
+
+function creaElementos(ite) {
+    let elem = document.createElement('li');
+    elem.classList.add('item');
+    elem.appendChild(ite);
+    lista.appendChild(elem);
+}
