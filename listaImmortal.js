0c67f5374840aea3befbb53d7aeb43caf47c117c
nahuel <mvkjdolgjsa11@gmail.com>
2022-06-22 15:58:34 -0300

    immortal 448

--- listaImmortal.js
+++ listaImmortal.js
@@ -13,6 +13,8 @@ var paginasT=0; //paginas totales
 var pagCargas=0
 var disponible=false;
 var arr = [
+['17', 'capitulo 448'],
+['27', 'capitulo 447'],
 ['19', 'capitulo 446'],
 ['27', 'capitulo 445'],
 ['28', 'capitulo 444'],
